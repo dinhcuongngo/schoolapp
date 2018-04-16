100c3f1f2270a2394ccfd505399c1a00050e3638
Cuong Ngo <dinhcuongngo@gmail.com>
2018-04-16 17:33:06 +0700

    COURSE: store() is finished!

---
+++ app/Http/Controllers/Course/CourseController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Course;
+
+use App\Course;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $courses = Course::all();
+
+        return response()->json(['data'=>$courses],200);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+
+        $course = Course::create($data);
+
+        return response()->json(['data'=>$course],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Course  $course
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Course $course)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Course  $course
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Course $course)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Course  $course
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Course $course)
+    {
+        //
+    }
+}
